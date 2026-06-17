e288a1e3822f35f1cf51c4c68b0bb648246faed4
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-17 23:01:18 +1000

    fix #4 Add Class model for SimultaneousEquation

---
+++ Core/Models/SimultaneousEquation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class SimultaneousEquation
+    {
+        public double A1 { get; set; }
+        public double B1 { get; set; }
+        public double C1 { get; set; }
+        public double A2 { get; set; }
+        public double B2 { get; set; }
+        public double C2 { get; set; }
+    }
+}
