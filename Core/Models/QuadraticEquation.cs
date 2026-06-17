13c59a19ffe893315e9c0b3e48b6b33385e83f93
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-17 22:58:53 +1000

    fix #3 Add class model for QuadraticEquation

---
+++ Core/Models/QuadraticEquation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class QuadraticEquation
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+    }
+}
