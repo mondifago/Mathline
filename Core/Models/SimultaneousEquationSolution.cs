ba62e523890f04aed123f3a957717ff7cb2b2246
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-17 23:06:06 +1000

    fix #7 add class mode for SimultaneousEquationSolution

---
+++ Core/Models/SimultaneousEquationSolution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class SimultaneousEquationSolution
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
