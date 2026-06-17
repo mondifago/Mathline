e36fb3ffbacb27d60580688760febfaf012d78e9
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-17 23:04:34 +1000

    fix #6 Add class model for QuadraticEquationSolution

---
+++ Core/Models/QuadraticEquationSolution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class QuadraticEquationSolution
+    {
+        public double Discriminant { get; set; }
+        public double SquareRootOfDiscriminant { get; set; }
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+    }
+}
