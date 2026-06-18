3462be86ae9f43a9df4f6deaab7977f075fcda95
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-18 15:43:53 +1000

    fix #9 Add SolveResult.cs Class to Models

---
+++ Core/Models/SolveResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class SolveResult<TSolution>
+    {
+        public TSolution Solution { get; set; }
+        public List<SolutionStep> Steps { get; set; } = new();
+        public string Method { get; set; }
+    }
+}
