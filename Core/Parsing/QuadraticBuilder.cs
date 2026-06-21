ad80083adb5820de8063ca1d58d4c9bfec024c55
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-21 23:44:45 +1000

    fix #27 Add QuadraticBuilder

---
+++ Core/Parsing/QuadraticBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Parsing
+{
+    public static class QuadraticBuilder
+    {
+        public static QuadraticEquation Build(EquationTerms terms)
+        {
+            double a = terms.Left.GetValueOrDefault("x^2") - terms.Right.GetValueOrDefault("x^2");
+            double b = terms.Left.GetValueOrDefault("x") - terms.Right.GetValueOrDefault("x");
+            double c = terms.Left.GetValueOrDefault("1") - terms.Right.GetValueOrDefault("1");
+
+            return new QuadraticEquation { A = a, B = b, C = c };
+        }
+    }
+}
