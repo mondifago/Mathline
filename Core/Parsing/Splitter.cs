0d5fc8f884dbab173b292cdd581a3e1c374bfa18
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-23 22:56:44 +1000

    fix #33 Add the Splitter

---
+++ Core/Parsing/Splitter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Parsing
+{
+    public static class Splitter
+    {
+        public static List<string> Split(string input)
+        {
+            var equations = new List<string>();
+
+            if (input is null) return equations;
+
+            foreach (string line in input.Split('\n'))
+            {
+                string trimmed = line.Trim();  
+                if (trimmed.Length > 0)
+                    equations.Add(trimmed);
+            }
+
+            return equations;
+        }
+    }
+}
