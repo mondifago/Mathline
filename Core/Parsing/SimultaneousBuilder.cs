b6e079d8df182cf0b7d2d7c1dfebe908fa0124aa
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-23 23:21:38 +1000

    fix #36 Add SimultaneousBuilder

---
+++ Core/Parsing/SimultaneousBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Parsing
+{
+    public static class SimultaneousBuilder
+    {
+        public static SimultaneousEquation Build(EquationTerms first, EquationTerms second)
+        {
+            return new SimultaneousEquation
+            {
+                A1 = NetVariable(first, "x"),
+                B1 = NetVariable(first, "y"),
+                C1 = NetConstant(first),
+                A2 = NetVariable(second, "x"),
+                B2 = NetVariable(second, "y"),
+                C2 = NetConstant(second)
+            };
+        }
+
+        private static double NetVariable(EquationTerms terms, string variable) =>
+            terms.Left.GetValueOrDefault(variable) - terms.Right.GetValueOrDefault(variable);
+
+        private static double NetConstant(EquationTerms terms) =>
+            terms.Right.GetValueOrDefault("1") - terms.Left.GetValueOrDefault("1");
+    }
+}
